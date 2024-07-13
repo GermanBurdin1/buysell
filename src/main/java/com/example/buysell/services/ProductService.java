81ecfdc0eaa18f7ea91dd9ca88843b644f4a67f9
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-07-13 22:22:31 +0200

    product service, model, controllers

---
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.example.buysell.services;
+
+import com.example.buysell.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> products = new ArrayList<>();
+    private long ID = 0;
+    {
+        products.add(new Product(++ID,"playStation 5","simple description", 500, "Metz","German"));
+        products.add(new Product(++ID,"Iphone 15","simple description",700, "Moscow","John"));
+    }
+    public List<Product> listProducts() {return products;}
+
+    public void saveProduct(Product product) {
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProduct(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+}
